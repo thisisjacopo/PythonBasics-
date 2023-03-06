b3db81cfc46b63b398fa1d5a517ad9f5fddb26f3
Jacopo <jacopo.rodighiero@icloud.com>
2023-03-06 20:22:23 +0100

    strings

--- HelloWorld/helloworld.py
+++ HelloWorld/helloworld.py
@@ -1,3 +1,7 @@
 # print() function and examples
 
 print('Hello, world!')
+print(1 + 2)
+print(7 * 6)
+print()
+print("the end")
---
+++ HelloWorld/stringsInPython.py
@@ -0,0 +1,12 @@
+# strings and variables
+
+print("today was a good day")
+print('we can include "quotes" in string')
+print("concatenate" + " strings")
+
+greeting = "Hello"
+
+# the value entered by the user will be assigned to the variable name
+name = input("Please enter your name ")
+
+print(greeting + " " + name)
